720981ed121ea9a049596c34ff7ce0bfff46635e
Naresha K <naresha.k@gmail.com>
2021-11-23 16:12:25 +0530

    Generate Swagger docs at build time using a JUnit test

---
+++ src/test/java/com/nareshak/demo/swaggerdemo/SwaggerGenerator.java
@@ -0,0 +1,34 @@
+package com.nareshak.demo.swaggerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerGenerator {
+
+    @Autowired
+    WebApplicationContext context;
+
+    @Test
+    public void generateSwagger() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
+                .andDo((result) -> {
+                    Files.write(Paths.get("swagger.json"),
+                            result.getResponse().getContentAsByteArray());
+                });
+
+    }
+}
